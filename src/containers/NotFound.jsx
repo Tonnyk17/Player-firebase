85c585c790d26889e9c89b7bc85d69ca91c1aba2
Tonyk <tonykurosaki117@gmail.com>
2021-03-26 16:30:03 -0600

    Notfound agregado

--- src/containers/NotFound.jsx
+++ src/containers/NotFound.jsx
@@ -1,8 +1,14 @@
 import React from "react";
+import "../assets/styles/containers/NotFound.css"
+
 
 const NotFound = () => (
-    <div>
-        Error 404 Not Found :/
+    <div className="Notfound">
+        <div className="text-container">
+          <img src="https://i.postimg.cc/VNy52t85/notfound.png" alt="cat image" className="notfound-image"/>
+                <h1 className="title main-title">404</h1>
+                <p>Page not found</p>
+        </div>
     </div>
 );
 
